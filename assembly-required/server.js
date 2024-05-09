ebb8513828a548a42713283b191fa8c1d97f29c4
Suveggies <jacobsuveges45@gmail.com>
2024-05-08 20:49:10 -0700

    Rate limiting and CSRF protection

--- assembly-required/server.js
+++ assembly-required/server.js
@@ -9,13 +9,26 @@ const PORT = 3000;
 app.disable('x-powered-by');
 app.use(bodyParser.text());
 
+const csurf = require("csurf");
+
 const limit = require("express-limit").limit;
 
-app.post('/run-assembly', 
-    limit({
-        max: 5, // 5 requests
-        period: 60 * 1000, // per minute (60 seconds)
-    }), (req, res) => {
+app.post('/run-assembly', (req, res) => {
+
+    const rateLimiter = require('../controls/rate-limiter')({
+
+        // Allow 3 submits, then slows down
+
+        freeRetries: 30,
+
+        minWait: 2 * 60 * 60,
+
+        maxWait: 2 * 60 * 60,
+
+        lifetime: 60 * 60
+    
+    });
+
     const assemblyCode = req.body;
 
     // Compile assembly code
@@ -36,6 +49,18 @@ app.post('/run-assembly',
             res.send(stdout);
         });
     }).stdin.write(assemblyCode);
+});
+
+app.use(csurf());
+
+
+
+app.use(function(req, res, next) {
+
+    res.cookie("mytoken", req.csrfToken());
+
+    next();
+
 });
 
 app.listen(PORT, () => {
